354f267a7017374fda2d58c008089bcba0d20987
Jakub Kozubal <kozubal.jakub@gmail.com>
2020-04-26 15:22:42 +0200

    Base Main menu view with missing functionalities

---
+++ Menu/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void SceneLoader(int index) {
+        SceneManager.LoadScene(index);
+    }
+}
---
+++ Menu/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+	[SerializeField] MenuButtonController menuButtonController;
+	[SerializeField] Animator animator;
+	[SerializeField] AnimatorFunctions animatorFunctions;
+	[SerializeField] int thisIndex;
+
+    // Update is called once per frame
+    public void Select()
+    {
+		animator.SetBool ("selected", true);
+		
+    }
+	public void Unselect()
+	{
+		animator.SetBool("selected", false);
+	}
+	public void Click(){
+		animator.SetBool ("pressed", true);
+	}
+	public void QuitGame(){
+		Debug.Log("quitting...");
+		Application.Quit();
+	}
+	public void PlaySound(AudioClip whichSound){
+		menuButtonController.audioSource.PlayOneShot(whichSound);
+	}
+}
