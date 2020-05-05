61b6041fc3a1988f206b266b1b99a3f4ba21ff90
Kamil Tyrek <tyrekkamil@gmail.com>
2020-05-05 22:31:56 +0200

    [fix] mouse was behind UI

--- Menu/Assets/Scripts/MouseCursor.cs
+++ Menu/Assets/Scripts/MouseCursor.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 
 public class MouseCursor : MonoBehaviour
 {
-    private SpriteRenderer rend;
-    public Sprite clickCursor;
-    public Sprite normalCursor;
+   // private SpriteRenderer rend;
+  // public Sprite clickCursor;
+  //  public Sprite normalCursor;
+  public Texture2D clickCursor;
+  public Texture2D normalCursor;
 
     void Start() {
-        Cursor.visible = false;
-        rend = GetComponent<SpriteRenderer>();
+        //Cursor.visible = false;\
+        Cursor.SetCursor(normalCursor, Vector2.zero, CursorMode.ForceSoftware);
     }
 
     void Update() {
-        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+               if(Input.GetMouseButtonDown(0)) {
+        Cursor.SetCursor(clickCursor, Vector2.zero, CursorMode.ForceSoftware);
+        } else if(Input.GetMouseButtonUp(0)) {
+        Cursor.SetCursor(normalCursor, Vector2.zero, CursorMode.ForceSoftware);
+        }
+ /*       Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = cursorPos;
-
+		
         if(Input.GetMouseButtonDown(0)) {
             rend.sprite = clickCursor;
         } else if(Input.GetMouseButtonUp(0)) {
             rend.sprite = normalCursor;
-        }
+        }*/
     }
 }
