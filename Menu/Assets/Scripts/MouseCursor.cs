5867232fa68ba3264ef1c468416e5be532fce1c2
Kamil Tyrek <tyrekkamil@gmail.com>
2020-05-05 22:34:54 +0200

    remove comments and format code

--- Menu/Assets/Scripts/MouseCursor.cs
+++ Menu/Assets/Scripts/MouseCursor.cs
@@ -4,30 +4,18 @@ using UnityEngine;
 
 public class MouseCursor : MonoBehaviour
 {
-   // private SpriteRenderer rend;
-  // public Sprite clickCursor;
-  //  public Sprite normalCursor;
   public Texture2D clickCursor;
   public Texture2D normalCursor;
 
     void Start() {
-        //Cursor.visible = false;\
         Cursor.SetCursor(normalCursor, Vector2.zero, CursorMode.ForceSoftware);
     }
 
     void Update() {
-               if(Input.GetMouseButtonDown(0)) {
-        Cursor.SetCursor(clickCursor, Vector2.zero, CursorMode.ForceSoftware);
-        } else if(Input.GetMouseButtonUp(0)) {
-        Cursor.SetCursor(normalCursor, Vector2.zero, CursorMode.ForceSoftware);
-        }
- /*       Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = cursorPos;
-		
         if(Input.GetMouseButtonDown(0)) {
-            rend.sprite = clickCursor;
+            Cursor.SetCursor(clickCursor, Vector2.zero, CursorMode.ForceSoftware);
         } else if(Input.GetMouseButtonUp(0)) {
-            rend.sprite = normalCursor;
-        }*/
+            Cursor.SetCursor(normalCursor, Vector2.zero, CursorMode.ForceSoftware);
+        }
     }
 }
