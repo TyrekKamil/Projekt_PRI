7cbbf031beaf08a373d3a1db03d888eb234062da
jkozubal <s0man3k@gmail.com>
2020-05-19 03:01:08 +0200

    Added initial character movement

---
+++ Menu/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    float horizontalMove = 0f;
+
+    public float moveSpeed = 20f;
+
+    bool jump = false;
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //movement of the character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
