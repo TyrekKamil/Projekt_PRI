d1538b4f1c18b9f4908337ad24b5be75f9c1d8e4
mhyps <45096147+mhyps@users.noreply.github.com>
2020-06-08 01:01:41 +0200

    Added working fullscreen and dynamic res

---
+++ Menu/Assets/Scripts/MenuOptions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+public class MenuOptions : MonoBehaviour
+{
+
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+    void Start()
+    {
+        resolutions = Screen.resolutions.Select(resolution => new Resolution { width = resolution.width, height = resolution.height }).Distinct().ToArray();
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentRes = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentRes = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentRes;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void SetfullScreen(bool isFS)
+    {
+        Screen.fullScreen = isFS;
+    }
+}
