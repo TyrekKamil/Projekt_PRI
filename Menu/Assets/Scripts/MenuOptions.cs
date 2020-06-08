79c2d3989763647dae607741dc45785872484e9c
Kamil Tyrek <tyrekkamil@gmail.com>
2020-06-08 20:12:57 +0200

    [added] vSync

--- Menu/Assets/Scripts/MenuOptions.cs
+++ Menu/Assets/Scripts/MenuOptions.cs
@@ -9,14 +9,22 @@ public class MenuOptions : MonoBehaviour
     public Dropdown resolutionDropdown;
 
     public Toggle fullScreen;
+
+    public Toggle vSync;
     Resolution[] resolutions;
     void Start()
     {
         resolutions = Screen.resolutions.Select(resolution => new Resolution { width = resolution.width, height = resolution.height }).Distinct().ToArray();
         resolutionDropdown.ClearOptions();
+
         bool fs = true;
+        bool vS = true;
+
         if(PlayerPrefs.GetString("FullScreen").Length == 1) {
             fs = PlayerPrefs.GetString("FullScreen") == "1" ? true : false;
+        } 
+        if(PlayerPrefs.GetString("VSync").Length == 1) {
+            vS = PlayerPrefs.GetString("VSync") == "1" ? true : false;
         }
         if(PlayerPrefs.GetString("Resolution").Contains("x")) {
             Screen.SetResolution(int.Parse(PlayerPrefs.GetString("Resolution").Split('x')[0]), 
@@ -41,7 +49,9 @@ public class MenuOptions : MonoBehaviour
         resolutionDropdown.value = currentRes;
         resolutionDropdown.RefreshShownValue();
         fullScreen.isOn = fs;
-        Debug.Log(PlayerPrefs.GetString("FullScreen"));
+        vSync.isOn = vS;
+        QualitySettings.vSyncCount = vS == true ? 1 : 0;
+        Debug.Log(QualitySettings.vSyncCount);
     }
 
     public void SetResolution(int resolutionIndex)
@@ -58,5 +68,6 @@ public class MenuOptions : MonoBehaviour
 
     public void SetVSync(bool isVSync) {
         QualitySettings.vSyncCount = isVSync == true ? 1 : 0;
+        PlayerPrefs.SetString("VSync", isVSync == true ? "1" : "0");
     }
 }
