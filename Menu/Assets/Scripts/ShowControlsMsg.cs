bd545f38fe5f809c7b5681883290c5beaa7d99af
Kamil Tyrek <tyrekkamil@gmail.com>
2020-06-08 19:04:04 +0200

    [improved] popup in tutorial show key from keybind

--- Menu/Assets/Scripts/ShowControlsMsg.cs
+++ Menu/Assets/Scripts/ShowControlsMsg.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ShowControlsMsg : MonoBehaviour
 {
     public GameObject uiObject;
+    Text text;
+
+    string left, right;
 
     // Start is called before the first frame update
     void Start()
     {
+        string left = PlayerPrefs.GetString("LeftButton").Replace("Arrow", "");
+        string right = PlayerPrefs.GetString("RightButton").Replace("Arrow", "");
         uiObject.SetActive(false);
+        text = uiObject.GetComponent<Text>();
+        text.text = "Press \"" +  left + "\" or \"" + right + "\" to move" ;
 
     }
     void OnTriggerEnter2D(Collider2D player)
@@ -27,4 +34,5 @@ public class ShowControlsMsg : MonoBehaviour
         Destroy(uiObject);
         // Destroy(gameObject);
     }
+
 }
