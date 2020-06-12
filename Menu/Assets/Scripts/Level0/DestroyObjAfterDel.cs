36b1097f0a7ea052f6b4f56d1dad8cb72770be46
mhyps <45096147+mhyps@users.noreply.github.com>
2020-06-12 02:15:32 +0200

    Destroy Objects for speedrun

---
+++ Menu/Assets/Scripts/Level0/DestroyObjAfterDel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjAfterDel : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            StartCoroutine("WaitForSec");
+        }
+    }
+
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+}
