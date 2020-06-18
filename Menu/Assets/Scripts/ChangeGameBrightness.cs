f84a51b462f86535806b919aed04dc9a9452b911
jkozubal <s0man3k@gmail.com>
2020-06-18 20:17:53 +0200

    Added brightness

---
+++ Menu/Assets/Scripts/ChangeGameBrightness.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class ChangeGameBrightness : MonoBehaviour
+{
+    public Slider brightness_Slider;
+
+    public static float intensityValue = 0.12f;
+    void Start()
+    {
+        intensityValue = brightness_Slider.value;
+    }
+    public void SetLightIntensity()
+    {
+        intensityValue = brightness_Slider.value;
+    }
+
+}
