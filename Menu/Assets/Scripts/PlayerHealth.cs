6cdfd45e74f3077e9f50bdc502697f697e891938
mhyps <45096147+mhyps@users.noreply.github.com>
2020-06-18 22:05:03 +0200

    update lightning and camer[LVL1]

---
+++ Menu/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private int health = 3;
+
+    public void ChangeHealth(int hit)
+    {
+        health -= hit;
+    }
+
+    public int DisplayHealth()
+    {
+        return health;
+    }
+}
