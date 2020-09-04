aaed8ab1c82b61e018c0f60bbf0d18665f48032a
Kamil Tyrek <tyrekkamil@gmail.com>
2020-09-04 19:51:59 +0200

    [improved] enemy follows player, distance script

--- Menu/Assets/Scripts/Enemy/EnemyAnimationController.cs
+++ Menu/Assets/Scripts/Enemy/EnemyAnimationController.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 public class EnemyAnimationController : MonoBehaviour
 {
     Animator anim;
     float moveSpeed = 2.5f;
 
-    float distance = 10;
+    float distance = 30;   
     bool move = false;
     public GameObject player;
 
     float direction = 1;
+    float prevDirection = 1;
+    float enemyPlayerDistance;
+
     public float leftEdge;
     public float rightEdge;
     void Start() {
@@ -19,24 +23,37 @@ public class EnemyAnimationController : MonoBehaviour
 
     void isReadyToMove() {
         if(move == true) {
-            return;
+            //return;
+            move = true;
         } else if(Mathf.Abs(player.transform.position.x - transform.position.x) < distance) {
             move = true;
         }
     }
 
     void reverse() {
-        if((transform.position.x <= leftEdge && direction == -1) || (transform.position.x >= rightEdge && direction == 1)) { 
-            transform.Rotate(0, 180, 0, 0);
-            direction *= -1;
-        } 
+        if (player.transform.position.x <= rightEdge && player.transform.position.x >= leftEdge) {
+            if (Mathf.Abs(player.transform.position.x - transform.position.x) < distance) { 
+                prevDirection = direction;
+                enemyPlayerDistance = transform.position.x - player.transform.position.x;
+                direction = enemyPlayerDistance > 0 ? -1 : 1;
+                if (prevDirection != direction /* && direction != 0 && Math.Abs(enemyPlayerDistance) > 0.1 */) { 
+                    transform.Rotate(0, 180, 0, 0); 
+                }
+            }
+        } else {
+            if (transform.position.x <= leftEdge || transform.position.x >= rightEdge) {
+                direction *= -1;
+                transform.Rotate(0, 180, 0, 0);
+            }
+        }
+ 
+
     }
     void Update() {
         reverse();
         isReadyToMove();
- /*       if(Input.GetKeyDown("1")) {
-            anim.Play("Rogue_death_01");
-        } else */ if(!anim.GetCurrentAnimatorStateInfo(0).IsName("Rogue_death_01") && move == true) {
+    
+        if(!anim.GetCurrentAnimatorStateInfo(0).IsName("Rogue_death_01") && move == true) {
             Vector3 movement = new Vector3(1f, 0f, 0f);
             transform.position += movement * Time.deltaTime * moveSpeed * direction;
             anim.Play("Rogue_walk_01");
