d1fe3509719dc0f0751a1472663acd4c2e5a6686
Kamil Tyrek <tyrekkamil@gmail.com>
2020-09-13 18:20:21 +0200

    [fix] missing script for attack enemy

---
+++ Menu/Assets/EnemyHP.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHP : MonoBehaviour
+{
+    public int maxHealth = 100;
+    int currentHealth;
+    public Animator animator;
+    void Start() {
+        currentHealth = maxHealth;
+    }
+
+
+    public void TakeDamage(int damage) {
+        currentHealth -= damage;
+        Debug.Log("Enemy hitted: " + currentHealth + " HP");
+        if(currentHealth <= 0) {
+            Die();
+        }
+    }
+
+    void Die() {
+        Debug.Log("I'm dead");
+        animator.Play("Rogue_death_01");
+        this.enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<RespawnPlayer>().enabled = false;
+        GetComponent<EnemyAnimationController>().enabled = false;
+    }
+
+}
