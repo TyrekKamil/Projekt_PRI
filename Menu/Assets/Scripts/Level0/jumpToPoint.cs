52ad091c2ddaf203a0203dae96a6544a119c530d
mhyps <45096147+mhyps@users.noreply.github.com>
2020-09-20 20:43:36 +0200

    fixes

--- Menu/Assets/Scripts/Level0/jumpToPoint.cs
+++ Menu/Assets/Scripts/Level0/jumpToPoint.cs
@@ -1,19 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class jumpToPoint : MonoBehaviour
 {
 
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
+    public GameObject uiObject;
+
     void OnTriggerEnter2D(Collider2D other)
     {
 
         if (other.CompareTag("Player"))
         {
             player.transform.position = respawnPoint.transform.position;
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
         }
 
+    }
+
+     IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(2);
+        uiObject.SetActive(false);
     }
 }
