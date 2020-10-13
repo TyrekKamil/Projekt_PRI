8b9c5940ff324d49a68d36e9ecb1a3b203165341
Kamil Tyrek <tyrekkamil@gmail.com>
2020-10-13 18:50:33 +0200

    [PPGWU-98] [PPGWU-95] Fix jumping on step boxes, adjusting map to step boxes quest, added box prefab and declaration of box layer

--- Menu/Assets/Scripts/BoxMoving.cs
+++ Menu/Assets/Scripts/BoxMoving.cs
@@ -7,12 +7,12 @@ public class BoxMoving : MonoBehaviour
     private Vector3 movement = new Vector3(1f, 0f, 0f);
     public float moveSpeed = 1.5f;
     public LayerMask wallLayer;
+    public LayerMask boxesLayer;
     public void MoveBox(int boxDirect) { 
         Collider2D[] walls = Physics2D.OverlapCircleAll(transform.position, 0.5f, wallLayer);
-        if(walls.Length == 0) {
-            transform.position += movement * Time.deltaTime * moveSpeed * boxDirect;    
+        Collider2D[] boxes = Physics2D.OverlapCircleAll(transform.position, 1.9f, boxesLayer);
+        if(walls.Length == 0 && boxes.Length == 1) {
+            transform.position += movement * Time.deltaTime * moveSpeed * boxDirect;
         }
     }
 }
-
-// todo nie przesuwa jak skacze 
