1257f63e79328c1924ce6d63144e8055ee959d95
Kamil Tyrek <tyrekkamil@gmail.com>
2020-10-14 18:08:03 +0200

    [PPGWU-102] [PPGWU-103] create graphic for puzzle, create staring, blank platform for puzzle - new scene Puzzle

---
+++ Menu/Assets/PuzzleGame/Scripts/Puzzle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour {
+    public int blocksPerLine = 3;
+
+    void Start() {
+        CreatePuzzle();
+    }
+    void CreatePuzzle() { 
+        for (int x = 0; x < blocksPerLine; x++) {
+            for (int y = 0; y < blocksPerLine; y++) {
+                GameObject blockObj = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                blockObj.transform.position = -Vector2.one * (blocksPerLine - 1) * 0.5f + new Vector2(x,y);
+                blockObj.transform.parent = transform;
+            }
+        }
+        Camera.main.orthographicSize = blocksPerLine * 0.55f;
+    }
+
+}
