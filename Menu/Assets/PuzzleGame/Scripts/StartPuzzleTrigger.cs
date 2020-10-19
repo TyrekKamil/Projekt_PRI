fcb8acfba48bea19a9d45bed85cb4522ce0fa1c4
Kamil Tyrek <tyrekkamil@gmail.com>
2020-10-19 19:29:44 +0200

    [PPGWU-109] [PPGWU-140] Add script for zooming camera on object. Add trigger for starting puzzle minigame.

---
+++ Menu/Assets/PuzzleGame/Scripts/StartPuzzleTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Cinemachine;
+
+public class StartPuzzleTrigger : MonoBehaviour {
+
+    private bool isZoomed = false;
+    public float smooth = 0.5f;
+    public GameObject virtCamera;
+    public Transform zoomingObject;
+    void OnTriggerStay2D (Collider2D col) {
+
+        if (Input.GetKey((KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("ActionButton")))) {
+            Debug.Log ("Start Camera zooming");
+            isZoomed = true;
+            virtCamera.GetComponent<CinemachineVirtualCamera>().Follow = zoomingObject;
+            virtCamera.SetActive(false);
+        }
+    }
+
+    void Update () {
+        if (isZoomed) {
+            Camera.main.orthographicSize = Camera.main.orthographicSize - Time.deltaTime * smooth;
+        } 
+        if (Camera.main.orthographicSize < 2) {
+            SceneManager.LoadScene(4);    
+        }
+    }
+}
