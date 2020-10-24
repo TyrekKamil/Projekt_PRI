5b8a1e1a4d9552d8c0e6e22a75943797652da5b9
Kamil Tyrek <tyrekkamil@gmail.com>
2020-10-24 18:10:45 +0200

    [PPGWU-147] [PPGWU-148] Minigame cannot be started once it has been completed. Trigger object is dark, when game is ended - in other case object has light.

--- Menu/Assets/PuzzleGame/Scripts/StartPuzzleTrigger.cs
+++ Menu/Assets/PuzzleGame/Scripts/StartPuzzleTrigger.cs
@@ -9,13 +9,20 @@ public class StartPuzzleTrigger : MonoBehaviour {
     public float smooth = 0.5f;
     public GameObject virtCamera;
     public Transform zoomingObject;
+    public GameObject lightPuzzle;
     void OnTriggerStay2D (Collider2D col) {
 
-        if (Input.GetKey((KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("ActionButton")))) {
+        if (Input.GetKey((KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("ActionButton"))) && !Statics.puzzleEnd) {
             Statics.recentPlayerPosition = GameObject.FindGameObjectsWithTag("Player")[0].transform.position;
             isZoomed = true;
             virtCamera.SetActive(false);
             Camera.main.transform.position = new Vector3(zoomingObject.position.x, zoomingObject.position.y, Camera.main.transform.position.z);
+        }
+    }
+
+    void Start() {
+        if (Statics.puzzleEnd) { 
+            lightPuzzle.SetActive(false);
         }
     }
 
