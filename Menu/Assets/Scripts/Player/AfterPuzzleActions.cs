25ea2ee75c7813bc7fdf94effa30a3c381fb2159
Kamil Tyrek <tyrekkamil@gmail.com>
2020-10-25 23:32:55 +0100

    [PPGWU-146] add missing for actions after puzzle

---
+++ Menu/Assets/Scripts/Player/AfterPuzzleActions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AfterPuzzleActions : MonoBehaviour
+{
+    void Start()
+    {
+        if (Statics.winPuzzle)
+        {
+            GetComponent<PlayerUIUpdates>().updateExperience(Statics.expAfterPuzzle);
+        }
+    }
+}
