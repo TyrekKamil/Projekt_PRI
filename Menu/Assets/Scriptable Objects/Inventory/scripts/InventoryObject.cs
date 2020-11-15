9b47147914d69c6e9688bd02e698ccaca9f375e4
mhyps <45096147+mhyps@users.noreply.github.com>
2020-11-15 19:57:13 +0100

    PPGWU-191

--- Menu/Assets/Scriptable Objects/Inventory/scripts/InventoryObject.cs
+++ Menu/Assets/Scriptable Objects/Inventory/scripts/InventoryObject.cs
@@ -17,21 +17,53 @@ public class InventoryObject : ScriptableObject
 
     public void AddItem(Item _item, int _amount)
     {
-        if(_item.buffs.Length > 0)
+        if (_item.buffs.Length > 0)
         {
-            Container.Items.Add(new InventorySlot(_item.Id, _item, _amount, false));
+            SetEmptySlot(_item, _amount);
             return;
 
         }
-        for (int i = 0; i < Container.Items.Count; i++)
+        for (int i = 0; i < Container.Items.Length; i++)
         {
-            if(Container.Items[i].item.Id == _item.Id)
+            if (Container.Items[i].ID == _item.Id)
             {
                 Container.Items[i].AddAmount(_amount);
                 return;
             }
         }
-        Container.Items.Add(new InventorySlot(_item.Id, _item, _amount, false));
+        SetEmptySlot(_item, _amount);
+    }
+    public InventorySlot SetEmptySlot(Item _item, int _amount)
+    {
+        for (int i = 0; i < Container.Items.Length; i++)
+        {
+            if (Container.Items[i].ID <= -1)
+            {
+                Container.Items[i].UpdateSlot(_item.Id, _item, _amount, false);
+                return Container.Items[i];
+            }
+        }
+
+        //TODO: set up a full inventory case
+        return null;
+    }
+
+    public void MoveItem(InventorySlot item1, InventorySlot item2)
+    {
+        InventorySlot temp = new InventorySlot(item2.ID, item2.item, item2.amount, false);
+        item2.UpdateSlot(item1.ID, item1.item, item1.amount, false);
+        item1.UpdateSlot(temp.ID, temp.item, temp.amount, false);
+    }
+
+    public void RemoveItem(Item _item)
+    {
+        for (int i = 0; i < Container.Items.Length; i++)
+        {
+            if (Container.Items[i].item == _item)
+            {
+                Container.Items[i].UpdateSlot(-1, null, 0, false);
+            }
+        }
     }
 
     public void Save()
@@ -48,7 +80,11 @@ public class InventoryObject : ScriptableObject
         {
             IFormatter formatter = new BinaryFormatter();
             Stream stream = new FileStream(string.Concat(Application.persistentDataPath, savePath), FileMode.Open, FileAccess.Read);
-            Container = (Inventory)formatter.Deserialize(stream);
+            Inventory newContainer = (Inventory)formatter.Deserialize(stream);
+            for (int i = 0; i < Container.Items.Length; i++)
+            {
+                Container.Items[i].UpdateSlot(newContainer.Items[i].ID, newContainer.Items[i].item, newContainer.Items[i].amount, false);
+            }
             stream.Close();
         }
     }
@@ -62,17 +98,33 @@ public class InventoryObject : ScriptableObject
 [System.Serializable]
 public class Inventory
 {
-    public List<InventorySlot> Items = new List<InventorySlot>();
+    public InventorySlot[] Items = new InventorySlot[32];
 }
 
 [System.Serializable]
 public class InventorySlot
 {
-    public int ID;
+    public int ID = -1;
     public Item item;
     public int amount;
     public bool isVisible;
+
+    public InventorySlot()
+    {
+        ID = -1;
+        item = null;
+        amount = 0;
+        isVisible = false;
+    }
     public InventorySlot(int _id,Item _item, int _amount, bool _isVisible)
+    {
+        ID = _id;
+        item = _item;
+        amount = _amount;
+        isVisible = _isVisible;
+    }
+
+    public void UpdateSlot(int _id, Item _item, int _amount, bool _isVisible)
     {
         ID = _id;
         item = _item;
