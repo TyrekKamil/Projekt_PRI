47df487be600d67844143817f6076fedb79f740e
Kamil Tyrek <tyrekkamil@gmail.com>
2020-12-21 19:31:04 +0100

    [PPGWU-269] missing files

---
+++ Menu/Assets/Scripts/Enemy/ExplodeBulletController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExplodeBulletController : MonoBehaviour
+{
+    public Rigidbody2D rigidbody;
+    private float scaleSpeed = 0.2f;
+    public LayerMask enemyLayers;
+
+    void Update()
+    {
+        if (gameObject.active)
+        {
+            transform.localScale = transform.localScale + new Vector3(scaleSpeed, scaleSpeed, scaleSpeed);
+            Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, 0.2f, enemyLayers);
+            foreach (Collider2D enemy in enemies)
+            {
+                enemy.GetComponent<EnemyHP>().TakeDamage(50);
+            }
+        }
+        if (transform.localScale.x > 45)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
