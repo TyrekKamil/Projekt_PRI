9eeebe51dfc2e120bed5cd26088ae300b5fee5ca
Kamil Tyrek <tyrekkamil@gmail.com>
2021-01-03 16:02:45 +0100

    [PPGWU-248] Cutscene added.

--- Menu/Assets/Scripts/Cutscene/CutsceneController.cs
+++ Menu/Assets/Scripts/Cutscene/CutsceneController.cs
@@ -4,11 +4,38 @@ using UnityEngine;
 
 public class CutsceneController : MonoBehaviour
 {
+    public GameObject[] subtitles;
+    public Animator teleportAnim;
     void Start()
     {
+        teleportAnim.SetBool("TeleportCutscene", false);
+        SecondScene();
     }
 
     void Update()
     {
+    }
+
+    public void SecondScene()
+    {
+        StartCoroutine(SecondSceneDelay());
+    }
+
+    IEnumerator SecondSceneDelay()
+    {
+        yield return new WaitForSeconds(5);
+        Camera.main.transform.position = new Vector3(-8.74f, 4.35f, -14f);
+        subtitles[0].SetActive(false);
+        subtitles[1].SetActive(true);
+        StartCoroutine(ThirdSceneDelay());
+    }
+
+        IEnumerator ThirdSceneDelay()
+    {
+        yield return new WaitForSeconds(5);
+        teleportAnim.SetBool("TeleportCutscene", true);
+        Camera.main.transform.position = new Vector3(-8.74f, 4.35f, -14f);
+        subtitles[1].SetActive(false);
+        subtitles[2].SetActive(true);
     }
 }
