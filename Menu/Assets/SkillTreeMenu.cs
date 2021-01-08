5fb849f1f86e5934622f9f1a23be17a5d44f41a1
jkozubal <kozubal.jakub@gmail.com>
2021-01-08 18:00:02 +0100

    [PPGWU-295] Opening skill tree with button

---
+++ Menu/Assets/SkillTreeMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillTreeMenu : MonoBehaviour
+{
+    private GameObject mouse;
+    public bool mouseOnScene = false;
+    private bool isSkillTreeOpen = false;
+    void Start()
+    {
+        mouse = GameObject.Find("normalCursor");
+        mouseOnScene = mouse.GetComponent<MouseCursor>().mouseOnScene;
+    }
+    [SerializeField]
+    private GameObject skillTreeMenu;
+    void Update()
+    {
+        if (Input.GetKeyDown((KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("SkillTreeButton")))) {
+            if (isSkillTreeOpen)
+            {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+        if (Input.GetKeyDown("escape"))
+        {
+            if (isSkillTreeOpen)
+            {
+                Resume();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        skillTreeMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isSkillTreeOpen = false;
+        mouse.GetComponent<MouseCursor>().SetVisibleCursor(mouseOnScene);
+    }
+
+    public void Pause()
+    {
+        skillTreeMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isSkillTreeOpen = true;
+        mouse.GetComponent<MouseCursor>().SetVisibleCursor(true);
+    }
+}
