f1d738ec3c604694f22feff2d4ee63de39d2590c
Kamil Tyrek <tyrekkamil@gmail.com>
2021-01-10 18:37:05 +0100

    [PPGWU-298] Skill icons only on screen when current skill is unlocked

---
+++ Menu/Assets/SkillTreeIcons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillTreeIcons : MonoBehaviour
+{
+    public GameObject increaseDmg;
+    public GameObject bullet;
+    public GameObject explode;
+    public GameObject regen;
+    public GameObject immorality;
+    public GameObject player;
+    void Start()
+    {
+        checkSkills();
+    }
+
+    public void checkSkills()
+    {
+        increaseDmg.SetActive(player.GetComponent<PlayerMovement>().CanUseIncreaseDMG());
+        bullet.SetActive(player.GetComponent<PlayerMovement>().canUseBullet());
+        explode.SetActive(player.GetComponent<PlayerMovement>().CanUseExplode());
+        regen.SetActive(player.GetComponent<PlayerMovement>().CanUseRegenHP());
+        immorality.SetActive(player.GetComponent<PlayerMovement>().CanUseImmortality());
+    }
+}
